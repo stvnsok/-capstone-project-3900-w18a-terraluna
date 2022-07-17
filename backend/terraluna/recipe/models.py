bf06975551447dcf6e2cbc8cc27ca94531f15a03
Michael Hua <z5257481@unsw.edu.au>
2022-07-18 05:50:32 +1000

    partial implementation ingredient2recipe

--- backend/terraluna/recipe/models.py
+++ backend/terraluna/recipe/models.py
@@ -1,3 +1,4 @@
+from setuptools import Require
 from app import db, logger
 
 
@@ -18,7 +19,7 @@ class Recipe(db.Model):
 
     @staticmethod
     def create(name, recipe_contributor, recipe_photo, description, meal_type, diet_type, 
-        recipe_instructions, timer_duration, timer_units, recipe2ingredient):
+        recipe_instructions, timer_duration, timer_units, required_ingredients):
         """Create a new recipe model and add it to the database. 
 
         Args:
@@ -31,7 +32,7 @@ class Recipe(db.Model):
             recipe_instructions (str): Recipe instructions.
             timer_duration (int): Cooking time quantity.
             timer_units (str): Cooking time units.
-            recipe2ingredient (dict): dictionary of ingredient_id and quantity
+            required_ingredients (dict): dictionary of ingredient_id and quantity
             
         Returns:
             Recipe: The new recipe model.
@@ -44,16 +45,17 @@ class Recipe(db.Model):
             timer_duration=timer_duration, timer_units=timer_units
         )
         db.session.add(recipe)
-        # TODO: add/modify recipe2ingredients table
+        # TODO: add/modify required_ingredients table
         #
         #
         #
+        RequiredIngredient.add_required_ingredients(recipe.id, required_ingredients)
         db.session.commit()
         logger.debug("Added recipe to DB: %s", recipe)  # type: ignore
         return
 
     def save(self, name, recipe_photo, description, meal_type, diet_type, 
-        recipe_instructions, timer_duration, timer_units, recipe2ingredient):
+        recipe_instructions, timer_duration, timer_units, required_ingredients):
         """Save recipe with new data.
 
         Args:
@@ -65,7 +67,7 @@ class Recipe(db.Model):
             recipe_instructions (str): Recipe instructions.
             timer_duration (int): Cooking time quantity.
             timer_units (str): Cooking time units.
-            recipe2ingredient (dict): dictionary of ingredient_id and quantity
+            required_ingredients (List(Dict)): list of (ingredient_id and quantity) required for recipe
             
         Returns:
             None
@@ -78,10 +80,12 @@ class Recipe(db.Model):
         self.recipe_instructions = recipe_instructions
         self.timer_duration = timer_duration
         self.timer_units = timer_units
-        # TODO: add/modify recipe2ingredients table
+
+        # TODO: add/modify required_ingredients table
         #
         #
         #
+        RequiredIngredient.update(required_ingredients)
 
         return True
     
@@ -120,3 +124,72 @@ class Ingredient(db.Model):
         db.session.commit()
         logger.debug("Added ingredient to DB: %s", ingredient)  # type: ignore
         return ingredient
+
+class RequiredIngredient(db.Model):
+    """Units and quantity of ingredients required for recipe"""
+
+    recipe_id = db.Column(db.ForeignKey('recipe.id'), primary_key=True)
+    ingredient_id = db.Column(db.ForeignKey('ingredient.id'), primary_key=True)
+
+    ingredient = db.relationship("Ingredient", back_populates="recipes")
+    recipe = db.relationship("Recipes", back_populates="required_ingredients")
+
+    required_quantity = db.Column(db.Integer)
+    required_units = db.Column(db.Text)
+
+    @staticmethod
+    def add_required_ingredients(recipe_id, required_ingredients):
+        """Add required ingredients for a recipe to the database
+
+        Args:
+            required_ingredients (List(Dict)): list of Dict(ingredient_id and quantity) required for recipe.
+
+        Returns:
+            None
+        
+        """
+        for required_ingredient in required_ingredients:
+            ingredient_id = required_ingredient['ingredient_id']
+            required_quantity = required_ingredient['required_quantity']
+            required_units = required_ingredient['required_units']
+            row = RequiredIngredient(recipe_id=recipe_id, ingredient_id=ingredient_id,
+                required_quantity=required_quantity, required_units=required_units)
+            db.session.add(row)
+
+    @staticmethod
+    def remove_all_recipe_ingredients(recipe_id):
+        """Remove all required ingredients for a recipe in the database
+
+        Args:
+            recipe_id (int): recipe to modified
+
+        Returns:
+            None
+        
+        """
+
+    @staticmethod
+    def update(recipe_id, required_ingredients):
+        """Update required ingredients for a recipe
+
+        Args:
+            required_ingredients (List(Dict)): list of Dict(ingredient_id and quantity) required for recipe.
+
+        Returns:
+            None
+        
+        """
+        # TODO: amend RequiredIngredients table to reflect accordingly
+        #
+        #
+        #       - Delete all records matching (recipe_id, ingredient_id)
+        #       - Add new records
+        #
+        #
+        requiredIngredientIDs = [requiredIngredient['ingredient_id'] for requiredIngredient in required_ingredients]
+        for ingredient_id in requiredIngredientIDs:
+            link = 
+            db.session.
+
+        RequiredIngredient.remove_all_recipe_ingredients(recipe_id)
+        RequiredIngredients.add_required_ingredients(recipe_id, required_ingredients)
