0cf54418f8978f7a7822dc174b3ac7fcb0a65bcb
James Kroeger <jameskroeger123@gmail.com>
2022-08-06 16:58:26 +1000

    finish script

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 # Start backend and frontend
 cd backend/ && . venv/bin/activate && flask run > /dev/null & pids=$!
-cd ../frontend/ && . ~/.bashrc && npm start & pids+=" $!"
+cd frontend/ && . ~/.bashrc && npm start & pids+=" $!"
 
 trap "kill $pids" SIGTERM SIGINT
 wait $pids
